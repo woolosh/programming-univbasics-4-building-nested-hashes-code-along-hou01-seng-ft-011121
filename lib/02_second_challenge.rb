609e388394596f1b98251a34e477631857c7d7f4
Joshua Morales <joshuapmorales@gmail.com>
2020-12-29 01:46:17 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -9,12 +9,14 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
+    }, 
    
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
+    }
+}
   
 end
